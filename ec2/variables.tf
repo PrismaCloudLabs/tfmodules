0d5ec709275ab1bd3472e3b8d90ece6b1a1a2ddf
Erick Moore <erick.moore@gmail.com>
2024-10-03 14:26:52 -0400

    added defaults

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -19,7 +19,8 @@ variable "name" {
 }
 
 variable "install_script" {
-  type = string
+  type    = string
+  default = "scripts/vulnerable.sh"
 }
 
 variable "instance_type" {
@@ -34,10 +35,12 @@ variable "private_ip" {
 
 variable "allowed_cidrs" {
   type = list(any)
+  default = [ "0.0.0.0/0", "10.0.0.0/8", "172.16.0.0/12" ]
 }
 
 variable "allowed_ports" {
   type = list(any)
+  default = [ 22, 80, 443, 9443, 3000, 8080 ]
 }
 
 variable "tags" {
